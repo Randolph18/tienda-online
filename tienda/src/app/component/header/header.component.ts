17b981cb06601ed44ff0cdeccb3cece7f1a20541
Randolph18 <randolph1808@gmail.com>
2023-01-19 19:21:39 -0300

    base de proyeto y primer commit

---
+++ tienda/src/app/component/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatIcon } from '@angular/material';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  imports: [ MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule ],
+})
+export class HeaderComponent {
+
+}
